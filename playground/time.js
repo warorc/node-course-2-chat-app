b21d45246a8c631667feb7513d73c9caab3c01bd
vadim.polosov <vadim.polosov.firefly@gmail.com>
2018-04-07 20:06:20 +0700

    timestmps

---
+++ playground/time.js
@@ -0,0 +1,15 @@
+var moment = require('moment');
+
+
+
+// var date = new Date();
+// var months = ['Jan', 'Feb']
+// console.log(date.getMonth());
+
+// var date = moment();
+// date.add(1, 'y').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+var date = moment();
+
+console.log(date.format('h:mm a'));
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,10 @@
+const moment = require('moment');
+
 var generateMessage = (from, text) => {
   return {
     from,
     text,
-    createdAt: new Date().getTime()
+    createdAt: moment().valueOf()
   };
 };
 
@@ -11,7 +13,7 @@ var generateLocationMessage = (from, lattitude, longitude) =>
   return {
     from,
     url: `https://www.google.com/maps?q=${lattitude},${longitude}`,
-    createdAt: new Date().getTime()
+    createdAt: moment().valueOf()
   };
 };
 
